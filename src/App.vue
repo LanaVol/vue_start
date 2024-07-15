78fb2c4c25eccb67132e6250af54f6067e323ed3
LanaVol <svolosh4yk@gmail.com>
2024-07-15 23:48:34 +0300

    init project & create basic count-switcher

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <button v-on:click="increaseCount">Increase count</button>
+    <button v-on:click="decreaseCount">Decrease count</button>
+    <div>
+      Amount of counts: <strong>{{ counts }}</strong>
+    </div>
+    <div>
+      Amount of numbers: <strong>{{ numbers }}</strong>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      counts: 5,
+      numbers: 10,
+    };
+  },
+  methods: {
+    increaseCount() {
+      this.counts += 1;
+    },
+    decreaseCount() {
+      this.counts -= 1;
+    },
+  },
+};
+</script>
+
+<style></style>
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from "vue";
+import App from "./App";
+
+// Cтворено екземпляр застосунку і вмонтовано в div id='app'
+createApp(App).mount("#app");
